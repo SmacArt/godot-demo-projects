d76885b4df3293453ec35c4a606bec0f70a41e38
Steve McCulloch <Smac_Art@hotmail.com>
2021-07-31 10:13:12 +1000

    get smetch class working

--- modules/smetch/smetch.cpp
+++ modules/smetch/smetch.cpp
@@ -1,25 +1,39 @@
 /* smetch.cpp */
 
 #include "smetch.h"
+#include <iostream>
 
-void Smetch::add(int p_value) {
-  count += p_value;
-}
-
-void Smetch::reset() {
-  count = 0;
-}
-
-int Smetch::get_total() const {
-  return count;
+Vector2 Smetch::get_mouse_position() {
+	return viewport->get_mouse_position();
 }
 
 void Smetch::_bind_methods() {
-  ClassDB::bind_method(D_METHOD("add", "value"), &Smetch::add);
-  ClassDB::bind_method(D_METHOD("reset"), &Smetch::reset);
-  ClassDB::bind_method(D_METHOD("get_total"), &Smetch::get_total);
+	ClassDB::bind_method(D_METHOD("get_mouse_position"), &Smetch::get_mouse_position);
+	ClassDB::bind_method(D_METHOD("create_canvas"), &Smetch::create_canvas);
 }
 
-Smetch::Smetch() {
-  count = 0;
+
+void Smetch::create_canvas(int x, int y) {
+	std::cout << "create canvas" << std::endl;
+
+  // todo : if this gets called again then need to clear the old objects
+
+  Size2 s2 = Size2(x, y);
+  this->set_size(s2);
+
+	viewport = memnew(SubViewport);
+	viewport->set_size_2d_override_stretch(true);
+  viewport->set_size(s2);
+
+	panel_container = memnew(PanelContainer);
+  panel_container->set_size(s2);
+
+	viewport->add_child(panel_container);
+	this->add_child(viewport);
+
+	std::cout << "create canvas end" << std::endl;
 }
+
+Smetch::Smetch(){}
+
+// todo : destructor
